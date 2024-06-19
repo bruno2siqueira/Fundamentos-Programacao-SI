e80514d47e41ab6c497fc827ac23e804fd23ec22
Bruno Siqueira da Silva <bruno2siqueira@users.noreply.github.com>
2024-06-19 20:27:06 -0300

    Create Atividade_2_Sistema_de_Cadastro_e_Verificacao_de_Senhas.c

---
+++ Atividade_2_Sistema de Cadastro_e_Verificacao_de_Senhas.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char password[20], input[20];
+    int attempt = 0;
+    int max_attempts = 3;
+
+    printf("Cadastro de Senha:\n");
+    printf("Digite sua nova senha: ");
+    scanf("%s", password);
+
+    printf("Senha cadastrada com sucesso!\n");
+
+    while (attempt < max_attempts) {
+        printf("Verificação de Senha (%d de %d tentativas):\n", attempt + 1, max_attempts);
+        printf("Digite sua senha: ");
+        scanf("%s", input);
+
+        if (strcmp(password, input) == 0) {
+            printf("Acesso concedido!\n");
+            return 0;
+        } else {
+            printf("Senha incorreta!\n");
+            attempt++;
+        }
+    }
+
+    printf("Número máximo de tentativas atingido. Acesso negado.\n");
+
+    return 0;
+}
