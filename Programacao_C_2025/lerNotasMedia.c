fc9795c36484f8bc29b96f87a70cf5c8e252e0bf
Bruno Siqueira da Silva <bruno2siqueira@users.noreply.github.com>
2025-04-28 19:44:29 -0300

    Add files via upload

---
+++ Programacao_C_2025/lerNotasMedia.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float n1, n2, n3, media;
+    scanf("%f", &n1);
+    scanf("%f", &n2);
+    scanf("%f", &n3);
+    // scanf("%f %f %f", &n1, &n2, &n3);
+    printf("Notas lidas: %f %.2f %.2f\n", n1, n2, n3);
+    return 0;
+}
