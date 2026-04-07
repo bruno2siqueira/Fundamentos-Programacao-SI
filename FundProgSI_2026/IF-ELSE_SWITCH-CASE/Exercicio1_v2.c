81cfbb7ea31ecbed7cad79a6cbc0092b8928f2e5
Bruno Siqueira da Silva <bruno2siqueira@users.noreply.github.com>
2026-04-07 19:52:34 -0300

    Add program to check if number is positive, negative, or zero

---
+++ FundProgSI_2026/IF-ELSE_SWITCH-CASE/Exercicio1_v2.c
@@ -0,0 +1,18 @@
+// 1) Fazer um programa para ler um número e 
+// imprimir uma mensagem informando se ele é positivo, negativo ou igual a zero. 
+
+#include <stdio.h>
+int main()
+{
+    int numero;
+    printf("Digite um numero: ");
+    scanf("%d", &numero);
+    
+    if(numero == 0) 
+        printf("Numero igual a ZERO");
+    else if(numero < 0)
+        printf("Numero menor que ZERO");
+    else
+        printf("Numero maior que ZERO");
+        
+}
