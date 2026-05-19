9fb787c13b8480c2c866933b62b647d6d21948db
Bruno Siqueira da Silva <bruno2siqueira@users.noreply.github.com>
2026-05-19 20:21:44 -0300

    Create matrizDiagonalPrincipal.c

---
+++ FundProgSI_2026/MATRIZES/matrizDiagonalPrincipal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define LIMITE 10
+int main()
+{
+    int l, c;         // indices que controlam as linhas e colunas da matriz
+    int matriz[LIMITE][LIMITE]; // matriz de 3 linhas e 3 colunas
+
+    // preciso agora de 2 lacos for para preencher a matriz
+    for (l = 0; l < LIMITE; l++)
+    { // linhas
+        for (c = 0; c < LIMITE; c++)
+        {                               
+            if(l == c){ // testar o valor elemento matriz[l][c]
+                matriz[l][c] = 1;
+            }else{
+                matriz[l][c] = 0;
+            }
+        }
+    }
+    for (l = 0; l < LIMITE; l++)
+    { // linhas
+        for (c = 0; c < LIMITE; c++)
+        {                                  // colunas
+            printf("%d \t", matriz[l][c]); // mostrar a matriz
+        }
+        printf("\n"); // quebra de linha após cada linha da matriz
+    }
+}
